1f4618111dab8f60fe95836d83da41e9571acb81
mahhis <vasiukovich03@gmail.com>
2024-10-02 16:13:12 +0300

    add ssl

--- vite.config.ts
+++ vite.config.ts
@@ -21,27 +21,25 @@ export default defineConfig({
     },
     outDir: 'dist',
   },
-  server: {
+  //server: {
     // https: {
     //   key: fs.readFileSync('/Users/mahhis/Downloads/fe1p.com/private.key'), // Your private key path
     //   cert: fs.readFileSync('/Users/mahhis/Downloads/fe1p.com/certificate.crt'), // Your certificate path
     //  ca: fs.readFileSync('/Users/mahhis/Downloads/fe1p.com/ca_bundle.crt'), // Your CA bundle path
     // },
     // host: '0.0.0.0', // Ensure server listens on all network interfaces
+ // },
+  server: {
+    https: {
+      key: fs.readFileSync('/etc/ssl/private/private.key'), // Your private key>
+      cert: fs.readFileSync('/etc/ssl/certificate.crt'), // Your certificate pa>
+      ca: fs.readFileSync('/etc/ssl/ca_bundle.crt'), // Your CA bundle path
+      // key: fs.readFileSync(path.resolve(__dirname, '/etc/ssl/fe1p.com/private.key')),
+      // cert: fs.readFileSync(path.resolve(__dirname, '/etc/ssl/fe1p.com/certificate.crt')),
+      // ca: fs.readFileSync('/etc/ssl/fe1p.com/ca_bundle.crt'), // Your CA bundle path
+
+    },  // Enable HTTPS
+    host: '0.0.0.0', // Ensure Vite listens on all network interfaces
   },
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync(path.resolve(__dirname, '/etc/ssl/fe1p.com/private.key')),
-  //     cert: fs.readFileSync(path.resolve(__dirname, '/etc/ssl/fe1p.com/certificate.crt')),
-  //     ca: fs.readFileSync('/etc/ssl/fe1p.com/ca_bundle.crt'), // Your CA bundle path
 
-  //   },  // Enable HTTPS
-  //   host: '0.0.0.0', // Ensure Vite listens on all network interfaces
-  // },
-
-
-
-  // key: fs.readFileSync('/etc/ssl/private/private.key'), // Your private key>
-  //     cert: fs.readFileSync('/etc/ssl/certificate.crt'), // Your certificate pa>
-  //     ca: fs.readFileSync('/etc/ssl/ca_bundle.crt'), // Your CA bundle path
 })
